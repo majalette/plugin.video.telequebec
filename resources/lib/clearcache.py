9bbc2f3940962631f59ba0122afca79645d8b38a
Marc-André Jalette <majalette@cegepadistance.ca>
2022-02-03 22:32:26 -0500

    print function in python3

--- resources/lib/clearcache.py
+++ resources/lib/clearcache.py
@@ -12,7 +12,7 @@ addon = Addon('plugin.video.telequebec')
 addon_cache_basedir = os.path.join(xbmc.translatePath(addon.getAddonInfo('path')).decode('utf-8'),".cache")
 
 if sys.argv[1].lower() == "full":
-    print "["+addon.getAddonInfo('name')+"] deleting full cache"
+    print("["+addon.getAddonInfo('name')+"] deleting full cache")
     for root, dirs, files in os.walk(addon_cache_basedir):
         for file in files:
             xbmcvfs.delete(os.path.join(root,file))
