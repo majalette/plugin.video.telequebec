2b9742bf4e8f2167929552a2c23854da95720c99
Marc-André Jalette <majalette@cegepadistance.ca>
2022-02-13 16:48:22 -0500

    fix live feed

--- resources/lib/navig.py
+++ resources/lib/navig.py
@@ -294,5 +294,5 @@ def obtenirMeilleurStream(pl,word='http'):
                     if res != None and res <= maxres:
                         maxBW = bandWidth
                         uri = line
-    log('LE BITRATE CHOISI EST ________%s' % maxBW)
+    log.log('LE BITRATE CHOISI EST ________%s' % maxBW)
     return uri
